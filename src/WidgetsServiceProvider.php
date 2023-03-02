1f23cb2b843681460ff96f94155575ac70a6f7f8
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-02 16:47:16 +0100

    Initial commit

---
+++ src/WidgetsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QRFeedz\Widgets;
+
+use Illuminate\Support\ServiceProvider;
+
+class WidgetsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
